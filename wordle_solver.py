27aedec1d19eebfcb2643489ecded46ffe3144bb
Derrin <wang.derrin@gmail.com>
2022-01-25 22:29:48 -0800

    Blueprint for the Wordle Assistant

---
+++ wordle_solver.py
@@ -0,0 +1,28 @@
+import argparse
+
+def wordPruning():
+    # prunes unapplicable words from the dictionary list
+    pass
+
+def letterParser():
+    # parses through a word to determine where a letter has been placed
+    pass
+
+def wordEntry():
+    # Process the user supplied info on (in)correct letter(s)
+    # ask how many letters were incorrect and which letter(s) then prune all words with these letters
+    # ask how many letters were correct but in the wrong place and which letter(s) then prune all words that have the letter in the wrong spot
+    # ask how many letters were correct and which letter(s) then prune all words that don't have the letters in the right spot
+    pass
+
+def randomWordGenerator():
+    # Generate a random word with the supplied information
+    pass
+
+def wordbankGenerator():
+    # Generate a wordbank with the supplied information
+    pass
+
+# import wordle dictionary into a list
+
+# Initial word entry user vs program supplied
